6fe7088331515ea86a9e5ab346ace9aa63120007
tempate <ddiazq28@gmail.com>
2019-07-01 03:39:16 +0200

    Implementation of alpha-beta pruning

--- src/search.c
+++ src/search.c
@@ -9,44 +9,57 @@ typedef struct {
 	int score;
 } Node;
 
-Node negamax(Board board, const int depth);
+Node alphabeta(Board board, const int depth, int alpha, const int beta);
 
 
 Move search(Board board, Settings settings) {
-	Node best = negamax(board, settings.depth);
+	Node best;
+
+	best = alphabeta(board, settings.depth, -MAX_SCORE - 1, MAX_SCORE + 1);
 
 	return best.move;
 }
 
-
-Node negamax(Board board, const int depth) {
+/*
+ * A basic implementation of alpha-beta pruning
+ * Checkmate in n moves is found on depth n+1
+ * alpha is the value to maximize and beta the value to minimize
+ */
+Node alphabeta(Board board, const int depth, int alpha, const int beta) {
 	Move moves[218];
 	History history;
 
 	Node node, best;
 
-	int k, newDepth;
+	int k;
 
 	if (depth == 0) {
 		best.score = eval(board);
 	} else {
 		k = legalMoves(board, moves);
-		newDepth = depth - 1;
 
 		if (k == 0) {
 			best.score = finalEval(board, depth);
 		} else {
-			best.score = -10001;
+			best.score = -MAX_SCORE - 1;
 
 			for (int i = 0; i < k; i++) {
 				makeMove(&board, moves[i], &history);
-				node = negamax(board, newDepth);
-				undoMove(&board, moves[i], history);
 
+				node = alphabeta(board, depth - 1, -beta, -alpha);
+
+				// Updates the best value and pruns the tree when alpha >= beta
 				if (-node.score > best.score) {
 					best.score = -node.score;
 					best.move = moves[i];
+
+					if (best.score >= beta) break;
+
+					if (best.score > alpha)
+						alpha = best.score;
 				}
+
+				undoMove(&board, moves[i], history);
 			}
 		}
 	}
