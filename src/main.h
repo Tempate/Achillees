156231b522fbf024938e72b326741d9944f83719
tempate <ddiazq28@gmail.com>
2019-07-10 04:28:07 +0200

    General updates

--- src/main.h
+++ src/main.h
@@ -18,21 +18,4 @@ enum {WHITE, BLACK};
 enum {PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING};
 enum {KCastle=1, QCastle=2, kCastle=4, qCastle=8};
 
-typedef struct {
-	uint64_t pieces[2][6];
-	uint64_t players[2];
-
-	uint64_t empty;
-	uint64_t occupied;
-
-	uint64_t key;
-
-	int turn;
-	int castling;
-	int enPassant;
-
-	int fiftyMoves;
-	int ply;
-} Board;
-
 #endif /* MAIN_H_ */
