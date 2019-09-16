87723ee2f6eab003b382c69ca44e9d84070f9be9
tempate <ddiazq28@gmail.com>
2019-09-17 00:38:50 +0200

    Removed PV struct

--- src/search.h
+++ src/search.h
@@ -1,18 +1,13 @@
 #ifndef SRC_SEARCH_H_
 #define SRC_SEARCH_H_
 
-#define MAX_DEPTH 50
+#define MAX_DEPTH 63
 #define DEF_DEPTH 5
 
 #define INFINITY 2 * MAX_SCORE
 #define R 2
 
 enum {EXACT, UPPER_BOUND, LOWER_BOUND};
-
-typedef struct {
-	Move moves[MAX_DEPTH];
-	int count;
-} PV;
 
 typedef struct {
 	uint64_t nodes;
